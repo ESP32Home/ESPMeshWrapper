0bf9bc83a544ccb22c0f5b010a202e466fae20ee
wassfila <wassim.filali@gmail.com>
2020-09-06 20:35:07 +0200

    added link to readme in the example

--- examples/main.ino
+++ examples/main.ino
@@ -1,8 +1,10 @@
-#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <ConfigUtils.h>
 
 #include <EspMeshWrapper.h>
+
+//see guthub repo readme for example config files and expected output log :
+//https://github.com/ESP32Home/ESPMeshWrapper/blob/master/README.md
 
 DynamicJsonDocument config(5*1024);//5 KB
 DynamicJsonDocument secret(1*1024);//1 KB
